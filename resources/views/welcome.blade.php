fa674e7430f82099fe49fbc04ee60f6aff0c7276
nderimrahmani <nderim.rahmani@gmail.com>
2022-05-26 20:50:43 +0200

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,7 +23,7 @@
     <body class="antialiased" >
     <div style="padding:20px">
         <br>
-        1. Clone the application from github into your local stream_socket_server  <br>
+        1. Clone the application from github into your local server  <br>
         2. Create a database and set the database name, username and password into your in the .inv file <br>
         3. run php artisan migrate <br>
         4. run php artisan db:seed <br>
